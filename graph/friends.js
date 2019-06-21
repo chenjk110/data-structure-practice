daa413917bf26210b0b266120aa6fdcc2f695cf9
chenjk110 <chenjk110@qq.com>
2019-06-21 15:55:09 +0800

    add friends graph example

---
+++ graph/friends.js
@@ -0,0 +1,43 @@
+class Person {
+  constructor(name) {
+    this.name = name
+
+    /* define */
+    Object.defineProperties(this, {
+      friends: {
+        enumerable: false,
+        configurable: false,
+        value: new Set()
+      }
+    })
+  }
+
+  get friendNames() {
+    return Array.from(this.friends).map(({ name }) => name)
+  }
+
+  get friendList() {
+    return Array.from(this.friends)
+  }
+  
+  addFriend(friend) {
+    if (friend === this) return
+    this.friends.add(friend)
+  }
+
+  removeFriend(friend) {
+    this.friends.delete(friend)
+  }
+  
+}
+
+
+const jack = new Person('Jack')
+const jane = new Person('Jane')
+const john = new Person('John')
+
+jack.addFriend(jane)
+jack.addFriend(john)
+
+console.log(jack.friendNames)
+console.log(jack.friendList)
